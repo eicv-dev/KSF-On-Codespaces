ecd11c950dd8f4e91b212a00bbbae557356ebe24
eicv-dev <admin@pusd.ws>
2026-05-23 13:53:28 -0700

    files

---
+++ install.sh
@@ -0,0 +1,19 @@
+pip install textual
+sleep 2
+python3 installer.py
+docker build -t ksf-on-codespaces . --no-cache
+
+sudo apt update
+sudo apt install -y jq
+
+mkdir Save
+cp -r root/config/* Save
+
+json_file="options.json"
+if jq ".enablekvm" "$json_file" | grep -q true; then
+    docker run -d --name=KSFOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE="KSF On Codespaces" -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped ksf-on-codespaces
+else
+    docker run -d --name=KSFOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE="KSF On Codespaces" -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped ksf-on-codespaces
+fi
+clear
+echo "KSF ON CODESPACES WAS INSTALLED SUCCESSFULLY! Check Port Tab"
